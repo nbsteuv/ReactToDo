cc7951aed68f22d19c5d5dcc7c845089dc481515
nbsteuv <nbsteuv@gmail.com>
2016-11-05 16:29:09 -0400

    Updated todosearch.jsx to use react-redux and updated todoapp.jsx accordingly

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,10 +2,9 @@ var React = require('react');
 var uuid = require('node-uuid');
 var moment = require('moment');
 
-// var TodoList = require('TodoList');
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
-var TodoSearch = require('TodoSearch');
+import TodoSearch from 'TodoSearch';
 var TodoAPI = require('TodoAPI');
 
 
